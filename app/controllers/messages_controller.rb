3b5f6b762f16f751274d716dfcf10c0aec5c6b32
taji <taji@sonicgarden.jp>
2026-02-03 22:24:59 +0900

    画面生成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
   def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.create(message_params)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to room_path(@room) }
+    end
+  end
+
+  private
+
+  def message_params
+    params.expect(message: [ :content, :sender_name ])
   end
 end
